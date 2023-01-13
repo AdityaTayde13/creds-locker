e334aad2663b2c1835de697e6ab6cd4e3e21e1aa
AdityaTayde13 <taydeaditya52@gmail.com>
2023-01-14 03:28:05 +0530

    updated database_manager.py

--- database_manager.py
+++ database_manager.py
@@ -13,4 +13,14 @@ def add(username, passhash):
        cursor.execute(createQuery)
 
        ## Insert encrypted username and password in the DB
-       
+       insertQuery  = "INSERT INTO credentials VALUES (?,?)"
+       cursor.execute(insertQuery,(username,passhash))
+       sqliteConnection.commit()
+# Handle errors
+   except sqlite3.Error as error:
+      sys.stderr.write("Error occured with adding login {}".format(error)) 
+   # Close DB Connection irrespective of success
+   # or failure
+   finally:
+       if sqliteConnection:
+           sqliteConnection.close()
