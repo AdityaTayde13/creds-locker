d3d1bbfcc6eeefee846c89e1b8db4b01cc02ee82
AdityaTayde13 <taydeaditya52@gmail.com>
2023-01-14 03:47:07 +0530

    updated database_manager1

--- database_manager.py
+++ database_manager.py
@@ -1,22 +1,39 @@
-import sqllite3
+import sqlite3
 import sys
 
 def add(username, passhash):
-    try:
+   try:
        # Connect to DB and create a cursor
-        sqliteConnection = sqlite3.connect('sql.db')
-        cursor = sqliteConnection.cursor()
+      sqliteConnection = sqlite3.connect('sql.db')
+      cursor = sqliteConnection.cursor()
 
         ## create table if  not exist
-
-       createQuery = "CREATE TABLE IF NOT EXISTS credentials (username TEXT , passhash TEXT)"
-       cursor.execute(createQuery)
-
+      createQuery = "CREATE TABLE IF NOT EXISTS credentials (username TEXT , passhash TEXT)"
+      cursor.execute(createQuery)
        ## Insert encrypted username and password in the DB
-       insertQuery  = "INSERT INTO credentials VALUES (?,?)"
-       cursor.execute(insertQuery,(username,passhash))
-       sqliteConnection.commit()
+      insertQuery  = "INSERT INTO credentials VALUES (?,?)"
+      cursor.execute(insertQuery,(username,passhash))
+      sqliteConnection.commit()
 # Handle errors
+   except sqlite3.Error as error:
+      sys.stderr.write("Error occured with adding login {}".format(error)) 
+   # Close DB Connection irrespective of success
+   # or failure
+   finally:
+       if sqliteConnection:
+           sqliteConnection.close()
+
+
+def view():
+   try:
+      # Connect to DB and create a cursor
+        sqliteConnection = sqlite3.connect('sql.db')
+        cursor = sqliteConnection.cursor()
+        selectQuery = "SELECT username, passhash FROM credentials"
+        cursor.execute(selectQuery)
+        result = cursor.fetchall()
+        return result
+   # Handle errors
    except sqlite3.Error as error:
       sys.stderr.write("Error occured with adding login {}".format(error)) 
    # Close DB Connection irrespective of success
